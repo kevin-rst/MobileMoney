5717d0d24c080f285fe3f7db2b1f04773ef24d9a
Kev-magic <kev30razafindramananatna@gmail.com>
2026-07-20 13:41:41 +0300

    logs

--- app/Views/layouts/frontoffice.php
+++ app/Views/layouts/frontoffice.php
@@ -25,6 +25,9 @@
                     <a class="mm-nav__primary" href="<?= site_url('/dashboard') ?>">Tableau de bord</a>
                     <a href="<?= base_url('client/solde/' . (session()->get('client_id') ?? '1') ) ?>">Mon solde</a>
                     <a href="<?= base_url('client/operations') ?>">Transaction</a>
+                    <form action="<?= site_url('/logout') ?>" method="post">
+                        <button type="submit">Se déconnecter</button>
+                    </form>
                 </nav>
             </div>
         </header>
